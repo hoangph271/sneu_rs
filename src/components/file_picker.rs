578bff0fdcd73aad5b803a11bf95920bab6b60e4
Phan Huy Hoang <hoangph271@gmail.com>
2022-08-28 05:29:21 +0700

    [Add] Scaffold "/music"

---
+++ src/components/file_picker.rs
@@ -0,0 +1,11 @@
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+pub struct FilePickerProps {}
+
+#[function_component(FilePicker)]
+pub fn file_picker(_props: &FilePickerProps) -> Html {
+    html! {
+        <div></div>
+    }
+}
---
+++ src/views/music.rs
@@ -0,0 +1,11 @@
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+pub struct MusicProps {}
+
+#[function_component(Music)]
+pub fn music(_: &MusicProps) -> Html {
+    html! {
+        <div></div>
+    }
+}
