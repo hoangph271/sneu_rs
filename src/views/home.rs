04b8cf3b6c87aa47daf4e22d127378c5e69f56ca
Phan Huy Hoang <hoangph271@gmail.com>
2022-08-28 03:58:15 +0700

    [Add] BulmaButton

--- src/views/home.rs
+++ src/views/home.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::Redirect;
 
+use crate::components::{BulmaButton, ButtonVariant};
 use crate::providers::{use_auth_context, AuthAction, AuthMessage};
 use crate::router::SneuRoute;
 
@@ -15,13 +16,12 @@ pub fn index() -> Html {
         AuthMessage::Authed(auth) => html! {
             <div>
                 <h4>{ format!("Welcome, {}...!", auth.username) }</h4>
-                <button
-                    class="button is-warning"
-                    type="button"
-                    onclick={move |_| {
-                        auth_context.dispatch(AuthAction::SignOut);
-                    }}
-                >{ "Sign out" }</button>
+                <BulmaButton
+                    variant={ButtonVariant::Warning}
+                    onclick={Callback::from(move |_| auth_context.dispatch(AuthAction::SignOut))}
+                >
+                    { "Sign out" }
+                </BulmaButton>
             </div>
         },
     }
