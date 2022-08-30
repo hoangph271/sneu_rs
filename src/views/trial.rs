a419d2cc55724a3a8c538952c98237e38c7584a4
Phan Huy Hoang <hoangph271@gmail.com>
2022-08-30 17:47:47 +0700

    [Add] Trial placeholder

--- src/views/trial.rs
+++ src/views/trial.rs
@@ -2,6 +2,11 @@ use yew::prelude::*;
 
 #[function_component(Trial)]
 pub fn trial() -> Html {
+    // use crate::components::*;
+    // html! {
+
+    // }
+
     html! {
         <code>
             { "TODO: // Add something to use SneuRoute::Trial" }
