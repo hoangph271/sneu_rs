c79cd95733a9bbfcaa76f411bd7c4796a0b4d106
Phan Huy Hoang <hoangph271@gmail.com>
2022-09-04 09:49:20 +0700

    [Code] Split

--- src/views/sneu_player/mod.rs
+++ src/views/sneu_player/mod.rs
@@ -1,12 +1,13 @@
-use core::panic;
+mod media_list;
+mod video_player;
 
+use crate::utils::expect_input_target;
+use core::panic;
 use gloo_file::{File, FileList};
-use wasm_bindgen_futures::spawn_local;
-use web_sys::HtmlVideoElement;
+use media_list::*;
+use video_player::*;
 use yew::prelude::*;
 use yew_hooks::use_state_ptr_eq;
-
-use crate::utils::{expect_input_target, no_op, setSrcObject};
 
 #[derive(Properties, PartialEq, Eq)]
 pub struct SneuPlayerProps {}
@@ -57,81 +58,6 @@ pub fn sneu_player(props: &SneuPlayerProps) -> Html {
             if let Some(opening_file) = (*opening_file).clone() {
                 <VideoPlayer file={opening_file} />
             }
-        </div>
-    }
-}
-
-#[derive(PartialEq, Properties)]
-pub struct MediaListProps {
-    files: FileList,
-    on_clicked: Callback<File>,
-}
-
-#[function_component(MediaList)]
-pub fn media_list(props: &MediaListProps) -> Html {
-    let MediaListProps { files, on_clicked } = props;
-
-    html! {
-        <ol>
-            {for files.iter().map(|file| {
-                html! {
-                    <li
-                        class="cursor-pointer"
-                        onclick={Callback::from({
-                            let on_clicked = on_clicked.clone();
-                            let file = (*file).clone();
-
-                            move |_| {
-                                on_clicked.emit(file.clone());
-                            }
-                        })}
-                        key={file.name()}
-                    >
-                        { file.name() }
-                    </li>
-                }
-            })}
-        </ol>
-    }
-}
-
-#[derive(PartialEq, Properties)]
-pub struct VideoPlayerProps {
-    pub file: File,
-}
-
-#[function_component(VideoPlayer)]
-pub fn video_player(props: &VideoPlayerProps) -> Html {
-    let VideoPlayerProps { file } = props;
-    let video_ref = use_node_ref();
-
-    use_effect_with_deps(
-        {
-            let video_ref = video_ref.clone();
-            let file = file.clone();
-
-            move |_| {
-                if let Some(video_el) = video_ref.cast::<HtmlVideoElement>() {
-                    spawn_local(async move {
-                        setSrcObject(&video_el, file.as_ref());
-                    });
-                }
-
-                no_op
-            }
-        },
-        file.clone(),
-    );
-
-    html! {
-        <div>
-            <h5>{ file.name() }</h5>
-            <video
-                style="max-width: 80vw; max-height: 400px;"
-                controls={true}
-                autoplay={true}
-                ref={video_ref.clone()}
-            />
         </div>
     }
 }
