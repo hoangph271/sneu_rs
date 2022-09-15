44b09f911503637f934dd72fe518abb30c0ad069
Phan Huy Hoang <hoangph271@gmail.com>
2022-09-15 12:03:35 +0700

    [Fix] use_lasted.rs @source_code url

--- src/components/use_lasted/wasted.rs
+++ src/components/use_lasted/wasted.rs
@@ -5,7 +5,7 @@ pub fn wasted() -> Html {
     html! {
         <a
             class="bg-gray-900/75 py-4 rounded-b text-center underline"
-            href="https://github.com/hoangph271/useLasted"
+            href="https://github.com/hoangph271/sneu_rs/blob/main/src/views/use_lasted.rs"
             target="_blank"
         >
             { "@source_code" }
--- src/views/use_lasted.rs
+++ src/views/use_lasted.rs
@@ -11,7 +11,7 @@ pub fn use_lasted(props: &UseLastedProps) -> Html {
     html! {
         <div
             class="w-screen h-screen bg-cover bg-no-repeat bg-center flex flex-col justify-center text-white px-2"
-            style="font-family: Monocraft; background-image: url(https://uselasted.netlify.app/static/media/751400.530ccd0e600c0697d435.png)"
+            style="font-family: Monocraft, monospace; background-image: url(https://uselasted.netlify.app/static/media/751400.530ccd0e600c0697d435.png)"
         >
             <Lasted />
             <Whys />
