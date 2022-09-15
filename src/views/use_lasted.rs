420b2b3f2cc05488f30ad7fbcad4e32264fdf5c4
Phan Huy Hoang <hoangph271@gmail.com>
2022-09-15 12:21:54 +0700

    [CSS]

--- src/views/use_lasted.rs
+++ src/views/use_lasted.rs
@@ -10,12 +10,16 @@ pub fn use_lasted(props: &UseLastedProps) -> Html {
 
     html! {
         <div
-            class="w-screen h-screen bg-cover bg-no-repeat bg-center flex flex-col justify-center text-white px-2"
+            class="w-screen h-screen bg-cover bg-no-repeat bg-center flex flex-col justify-center"
             style="font-family: Monocraft, monospace; background-image: url(https://uselasted.netlify.app/static/media/751400.530ccd0e600c0697d435.png)"
         >
-            <Lasted />
-            <Whys />
-            <Wasted />
+            <div
+                class="max-w-fit m-auto flex flex-col text-white px-2"
+            >
+                <Lasted />
+                <Whys />
+                <Wasted />
+            </div>
         </div>
     }
 }
