bf7a904196d0032955e7b6e97b9e9dfe01096aac
Phan Huy Hoang <hoangph271@gmail.com>
2022-09-20 00:16:01 +0700

    [Text] whys.rs

--- src/components/use_lasted/whys.rs
+++ src/components/use_lasted/whys.rs
@@ -1,10 +1,9 @@
 use yew::prelude::*;
 
-const WHYS: [&str; 4] = [
+const WHYS: [&str; 3] = [
     "Stay brown for 30 days, and THEN back to green...! :\"}",
     "Really, it makes me feel so tired sometimes...!",
-    "Cuz you\'ve been well behaved for the last 9 days, keep it going...!",
-    "🍀",
+    "Cuz you\'re half way there...!",
 ];
 
 #[function_component(Whys)]
@@ -14,7 +13,7 @@ pub fn whys() -> Html {
             { WHYS.iter().map(|why| {
                 html! {
                     <li key={*why}>
-                        { format!("• {why}") }
+                        { format!("🍀 {why}") }
                     </li>
                 }
             }).collect::<Html>() }
