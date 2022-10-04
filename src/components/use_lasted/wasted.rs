9390245f7dbff9de542e887c14aa344d3394b48e
Phan Huy Hoang <hoangph271@gmail.com>
2022-10-05 00:09:40 +0700

    [Add] #Lasted 30 days

--- src/components/use_lasted/wasted.rs
+++ src/components/use_lasted/wasted.rs
@@ -3,12 +3,10 @@ use yew::prelude::*;
 #[function_component(Wasted)]
 pub fn wasted() -> Html {
     html! {
-        <a
-            class="bg-gray-900/75 py-4 rounded-b text-center underline"
-            href="https://github.com/hoangph271/sneu_rs/blob/main/src/views/use_lasted.rs"
-            target="_blank"
+        <div
+            class="bg-gray-900/75 py-4 rounded-b text-center"
         >
-            { "@source_code" }
-        </a>
+            { "#DONE" }
+        </div>
     }
 }
